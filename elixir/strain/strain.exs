35559149b1496affbf933f0df96efdb61d2f2256
Daniel Schömer <daniel.schoemer@gmx.net>
2018-08-10 21:13:00 +0200

    Elixir/Strain, first iteration

---
+++ elixir/strain/strain.exs
@@ -0,0 +1,31 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep([], _), do: []
+  def keep([head | tail], fun) do
+    cond do
+      fun.(head) -> [head | keep(tail, fun)]
+      true       -> keep(tail, fun)
+    end
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard([], _), do: []
+  def discard([head | tail], fun) do
+    cond do
+      fun.(head) -> discard(tail, fun)
+      true       -> [head | discard(tail, fun)]
+    end
+  end
+end
