bcad67faa1600173d345bd86c091c97c38cffc8e
Daniel Schömer <daniel.schoemer@gmx.net>
2018-08-10 21:45:03 +0200

    Elixir/Protein Translation: First implementation of of_codon()

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -4,7 +4,10 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    Regex.split(~r/.../u, rna, include_captures: true, trim: true)
   end
+
+  def of_rna()
 
   @doc """
   Given a codon, return the corresponding protein
@@ -28,6 +31,22 @@ defmodule ProteinTranslation do
   UGA -> STOP
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon("UGU"), do: {:ok, "Cysteine"}
+  def of_codon("UGC"), do: {:ok, "Cysteine"}
+  def of_codon("UUA"), do: {:ok, "Leucine"}
+  def of_codon("UUG"), do: {:ok, "Leucine"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UUU"), do: {:ok, "Phenylalanine"}
+  def of_codon("UUC"), do: {:ok, "Phenylalanine"}
+  def of_codon("UCU"), do: {:ok, "Serine"}
+  def of_codon("UCC"), do: {:ok, "Serine"}
+  def of_codon("UCA"), do: {:ok, "Serine"}
+  def of_codon("UCG"), do: {:ok, "Serine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon("UAU"), do: {:ok, "Tyrosine"}
+  def of_codon("UAC"), do: {:ok, "Tyrosine"}
+  def of_codon("UAA"), do: {:ok, "STOP"}
+  def of_codon("UAG"), do: {:ok, "STOP"}
+  def of_codon("UGA"), do: {:ok, "STOP"}
+  def of_codon(_),     do: {:error, "invalid codon"}
 end
