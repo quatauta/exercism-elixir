d016b41a59c5047a740559806d923c8892f4a9c4
Daniel Schömer <daniel.schoemer@gmx.net>
2018-08-27 19:58:44 +0200

    Elixir/Acronym: Second iteration using Regex.scan/3

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,19 +5,6 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    string |> split |> filter_words |> abbreviate_words |> upcase
+    Regex.scan(~r/\b\pL|\p{Lu}/, string) |> Enum.join |> String.upcase
   end
-
-  defp split(string) do
-    String.split(string, ~r/(\p{Lu}+|\p{Lu}?\p{Ll}+)(?=\p{Lu}|\b)/, include_captures: true)
-  end
-
-  defp filter_words(list), do: Enum.filter(list, fn word -> word =~ ~r/^\p{L}/ end)
-
-  defp abbreviate_words([head | tail]), do: abbreviate_word(head) <> abbreviate_words(tail)
-  defp abbreviate_words([]), do: ""
-
-  defp abbreviate_word(word), do: word |> String.first
-
-  defp upcase(string), do: String.upcase(string)
 end
