173140d661eb3e3ac5fa894723c74630c86ef50b
Daniel Schömer <daniel.schoemer@gmx.net>
2018-09-18 20:49:08 +0200

    Elixir/Series: First iteration

--- elixir/series/series.exs
+++ elixir/series/series.exs
@@ -5,6 +5,14 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
-  def slices(_s, _size) do
+  def slices("", _), do: []
+  def slices(_, size) when size < 1, do: []
+  def slices(s, 1), do: String.graphemes(s)
+  def slices(s, size) do
+    cond do
+      String.length(s) >= size -> [ String.slice(s, 0, size) |
+                                    slices(String.slice(s, 1, String.length(s) - 1), size) ]
+      true -> []
+    end
   end
 end
