51739b049c2526ea497a1ba26c556195418773c1
Daniel Schömer <daniel.schoemer@gmx.net>
2018-09-26 21:29:52 +0200

    Elixir/Run Length Encoding: First iteration

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,10 +7,40 @@ defmodule RunLengthEncoder do
   "2A3B4C" => "AABBBCCCC"
   """
   @spec encode(String.t()) :: String.t()
+  def encode(nil), do: ""
+  def encode(""), do: ""
   def encode(string) do
+    {head, tail} = String.next_grapheme(string)
+    encode_helper(tail, head, 1)
+  end
+
+  defp encode_helper("", grapheme, count), do: encode_print(grapheme, count)
+
+  defp encode_helper(string, grapheme, count) do
+    {head, tail} = String.next_grapheme(string)
+
+    if grapheme == head do
+      encode_helper(tail, grapheme, count + 1)
+    else
+      encode_print(grapheme, count) <> encode(string)
+    end
+  end
+
+  defp encode_print(grapheme, 1), do: grapheme
+  defp encode_print(grapheme, count) do
+    Integer.to_string(count) <> grapheme
   end
 
   @spec decode(String.t()) :: String.t()
+  def decode(""), do: ""
   def decode(string) do
+    Regex.scan(~r/(\d*)(\D)/i, string)
+    |> Enum.map(fn e -> [_, n, s] = e ; decode_print(n, s) end)
+    |> Enum.join
   end
+
+  defp decode_print("", string), do: string
+  defp decode_print(0, _), do: ""
+  defp decode_print(1, string), do: string
+  defp decode_print(n, string), do: String.duplicate(string, String.to_integer(n))
 end
