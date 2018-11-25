71f40615aca44320de9452a4b6ab4d25e23a634f
Daniel Schömer <daniel.schoemer@gmx.net>
2018-11-25 12:14:51 +0100

    Elixir/Binary Search: First iteration

--- elixir/binary-search/binary_search.exs
+++ elixir/binary-search/binary_search.exs
@@ -18,6 +18,25 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search({}, _), do: :not_found
   def search(numbers, key) do
+    search(numbers, key, 0, tuple_size(numbers) - 1)
+  end
+
+  defp search({}, _, _, _), do: :not_found
+  defp search(_, _, first, _) when first < 0, do: :not_found
+  defp search(_, _, first, last) when first > last, do: :not_found
+  defp search(numbers, _, _, last) when last >= tuple_size(numbers), do: :not_found
+  defp search(numbers, key, first, _) when key < elem(numbers, first), do: :not_found
+  defp search(numbers, key, _, last) when key > elem(numbers, last), do: :not_found
+  defp search(numbers, key, first, last) do
+    median_position = first + div(last - first, 2)
+    median = elem(numbers, median_position)
+
+    cond do
+      key == median -> {:ok, median_position}
+      key < median -> search(numbers, key, first, median_position - 1)
+      key > median -> search(numbers, key, median_position + 1, last)
+    end
   end
 end
