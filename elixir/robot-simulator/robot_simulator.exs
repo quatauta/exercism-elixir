0477a92837da32761fb88be0a8fd360b09349905
Daniel Schömer <daniel.schoemer@gmx.net>
2019-02-24 21:06:29 +0100

    Elixir/Robot Simulator: Fourth iteration

--- elixir/robot-simulator/robot_simulator.exs
+++ elixir/robot-simulator/robot_simulator.exs
@@ -1,17 +1,27 @@
 defmodule RobotSimulator do
+  defguard is_direction(direction) when direction == :north or direction == :east or direction == :south or direction == :west
+  defguard is_position(position) when tuple_size(position) == 2 and is_integer(elem(position, 0)) and is_integer(elem(position, 1))
+  defp valid_instruction?("R"), do: true
+  defp valid_instruction?("L"), do: true
+  defp valid_instruction?("A"), do: true
+  defp valid_instruction?(_), do: false
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ :north, position \\ {0, 0}) do
-    cond do
-      !valid_direction?(direction) -> {:error, "invalid direction"}
-      !valid_position?(position) -> {:error, "invalid position"}
-      true -> { direction, position }
-    end
+  def create(direction \\ :north, position \\ {0, 0})
+  def create(direction, position) when is_direction(direction) and is_position(position) do
+    { direction, position }
   end
+  def create(direction, _) when not is_direction(direction), do: { :error, "invalid direction" }
+  ####
+  # def create(_, position) when not is_position(position), do: { :error, "invalid position" }
+  # causes "no function clause matching in RobotSimulator.create/2"
+  # with create(:north, "invalid")
+  def create(_, _), do: { :error, "invalid position" }
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -32,6 +42,7 @@ defmodule RobotSimulator do
   def simulate({:east,  {x, y}}, "A"), do: {:east,  {x + 1, y}}
   def simulate({:south, {x, y}}, "A"), do: {:south, {x, y - 1}}
   def simulate({:west,  {x, y}}, "A"), do: {:west,  {x - 1, y}}
+
   def simulate(robot, instructions) do
     {instruction, rest} = String.next_grapheme(instructions)
 
@@ -41,11 +52,6 @@ defmodule RobotSimulator do
     end
   end
 
-  defp valid_instruction?("R"), do: true
-  defp valid_instruction?("L"), do: true
-  defp valid_instruction?("A"), do: true
-  defp valid_instruction?(_), do: false
-
   @doc """
   Return the robot's direction.
 
@@ -54,18 +60,9 @@ defmodule RobotSimulator do
   @spec direction(robot :: any) :: atom
   def direction({ direction, _ }), do: direction
 
-  defp valid_direction?(:north), do: true
-  defp valid_direction?(:east),  do: true
-  defp valid_direction?(:south), do: true
-  defp valid_direction?(:west),  do: true
-  defp valid_direction?(_),      do: false
-
   @doc """
   Return the robot's position.
   """
   @spec position(robot :: any) :: {integer, integer}
   def position({ _, position }), do: position
-
-  defp valid_position?({x, y}) when is_integer(x) and is_integer(y), do: true
-  defp valid_position?(_), do: false
 end
