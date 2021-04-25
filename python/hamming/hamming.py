88ce387e203b0cf4b1fd65bc88507614c255131e
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-25 20:07:10 +0200

    Implemented Python exercise Hamming with string character comparision

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,11 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands A & B have different lengths. A: {len(strand_a)}, B: {len(strand_b)}")
+
+    distance = 0
+
+    for index in range(0, len(strand_a)):
+        if strand_a[index] != strand_b[index]:
+            distance += 1
+
+    return distance
