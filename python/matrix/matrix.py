bb3902bd4d33506b117de1c702d5432e4cdaa3a9
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-25 19:52:49 +0200

    Implemented Pyhton exercise Matrix with two-dimensional list of integers

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,20 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.rows = []
+        lines = matrix_string.splitlines()
+
+        for line in lines:
+            columns = [int(value) for value in line.split()]
+
+            self.rows.append(columns)
 
     def row(self, index):
-        pass
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
+        column = []
+
+        for row in self.rows:
+            column.append(row[index - 1])
+
+        return column
