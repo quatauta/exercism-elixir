ced621802de5851215eb14e50b7b09f76dd74659
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-26 19:02:17 +0200

    Implemented Python exercise Hamming with list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,10 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands A & B have different lengths. A: {len(strand_a)}, B: {len(strand_b)}")
 
-    distance = 0
-
-    for index in range(0, len(strand_a)):
-        if strand_a[index] != strand_b[index]:
-            distance += 1
-
-    return distance
+    return len([1 for a, b in zip(strand_a, strand_b) if a != b])
