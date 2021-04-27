53da5d09ab684d21f68409980cd1c0bb03306234
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-27 21:59:41 +0200

    Implemented Python exercise Word Count using regular expressions and list comprehension

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
+import re
+import string
+
 def count_words(sentence):
-    pass
+    words = re.split("[^a-z0-9']", sentence.lower())
+    words = [re.sub("(^'+|'+$)", "", w) for w in words]
+    words = [w for w in words if w]
+    counts = {}
+
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+    return counts
