8a82a6d63f30a5243983bf15568cfff92a661650
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-27 21:36:24 +0200

    Implemented Python exercise Isogram using list comprehension and set

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,10 +1,7 @@
 import string
 
 def is_isogram(text):
-    chars = {}
+    chars = [c for c in text.lower() if c in string.ascii_lowercase]
+    unique = set(chars)
 
-    for char in text.lower():
-        if char in string.ascii_lowercase:
-            chars[char] = chars.get(char, 0) + 1
-
-    return len([x for x in chars.values() if x > 1]) == 0
+    return len(chars) == len(unique)
