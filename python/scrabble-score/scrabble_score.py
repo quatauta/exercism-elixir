12e29655d37aab10f40bd4039267350043d0b97f
Daniel Schömer <daniel.schoemer@gmx.net>
2021-04-30 22:27:19 +0200

    Implemented Python exercise Scrabble Score with map/reduce

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,19 @@
+from collections import Counter
+from functools import *
+import string
+
 def score(word):
-    pass
+    letter_count = Counter(letters(word.lower()))
+    letter_scores = map(lambda x: x[1] * letter_score(x[0]), letter_count.items())
+    total_score = reduce(lambda x, y: x + y, letter_scores, 0)
+
+    return total_score
+
+def letter_score(letter):
+    letter_classes = { "aeioulnrst": 1, "dg": 2, "bcmp": 3, "fhvwy": 4, "k": 5, "jx": 8, "qz": 10 }
+    score = [score for letters, score in letter_classes.items() if letter in letters][0]
+
+    return score
+
+def letters(word):
+    return list(filter(lambda letter: letter in string.ascii_letters, list(word)))
