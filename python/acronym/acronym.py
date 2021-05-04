55c93330cf6b65e80300b8938b8e263c2c3f871a
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-04 21:42:19 +0200

    Implemented Python exercise Acronym using regular expressions

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,9 @@
+import re
+
 def abbreviate(words):
-    pass
+    words = re.sub(r"[_']+", "", words)
+    letters = re.compile(r"\b[a-zA-Z]").findall(words)
+    letters = map(lambda x: x.upper(), letters)
+    acronym = "".join(letters)
+
+    return acronym
