ec8bc2bde926dbbc0298edd0485b76b8828c4a4a
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-08 07:27:17 +0200

    Moved solution into lib/

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -8,6 +8,10 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-  end
+  def to_rna([head | tail ]), do: to_rna(head) ++ to_rna(tail)
+  def to_rna([]), do: ''
+  def to_rna(?A), do: 'U'
+  def to_rna(?T), do: 'A'
+  def to_rna(?C), do: 'G'
+  def to_rna(?G), do: 'C'
 end
