d58e06b94ec1b5d30a42245ce029461291d0e347
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-08 09:03:15 +0200

    Updated Elixir exercise Word Count

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,22 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    downcase(sentence) |> split() |> count_words()
+  end
+
+  defp downcase(sentence) do
+    String.downcase(sentence)
+  end
+
+  defp split(sentence) do
+    String.split(sentence, ~r/[^\pL\pN-]/u, trim: true)
+  end
+
+  defp count_words(words) do
+    Enum.reduce(words, %{}, fn(word, count) -> increment(word, count) end)
+  end
+
+  def increment(word, count) do
+    Map.update(count, word, 1, fn(word_count) -> word_count + 1 end)
   end
 end
