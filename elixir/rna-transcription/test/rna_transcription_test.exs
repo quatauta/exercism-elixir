c0a87b15bd9841a8cf65f771f8ccb9aafd8ed668
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-08 07:28:30 +0200

    Enabled full RNS Transcription test suite

--- elixir/rna-transcription/test/rna_transcription_test.exs
+++ elixir/rna-transcription/test/rna_transcription_test.exs
@@ -6,22 +6,22 @@ defmodule RnaTranscriptionTest do
     assert RnaTranscription.to_rna('G') == 'C'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes cytosine to guanine" do
     assert RnaTranscription.to_rna('C') == 'G'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes thymidine to adenine" do
     assert RnaTranscription.to_rna('T') == 'A'
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes adenine to uracil" do
     assert RnaTranscription.to_rna('A') == 'U'
   end
 
-  @tag :pending
+  # @tag :pending
   test "it transcribes all dna nucleotides to rna equivalents" do
     assert RnaTranscription.to_rna('ACGTGGTCTTAA') == 'UGCACCAGAAUU'
   end
