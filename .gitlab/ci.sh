cae774cf5dbbf954847c1c5dfa89cacd4c119a05
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-09 16:54:23 +0200

    Added .gitlab/ci.sh to collect CI/CD bash/shell helper functions

---
+++ .gitlab/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ci_section_begin() {
+    SECTION="$1"
+    DESCRIPTION="$2"
+    echo -e "\e[0Ksection_start:$(date +%s):${SECTION}\r\e[0K${DESCRIPTION:-SECTION}"
+}
+
+ci_section_end() {
+    SECTION="$1"
+    echo -e "\e[0Ksection_end:$(date +%s):${SECTION}\r\e[0K"
+}
