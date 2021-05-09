66dc293567ce20f5cbba7edc6fa7cc3fea9a2274
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-09 16:56:52 +0200

    Fixed CI helper function ci_section_begin description parameter

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -3,7 +3,7 @@
 ci_section_begin() {
     SECTION="$1"
     DESCRIPTION="$2"
-    echo -e "\e[0Ksection_start:$(date +%s):${SECTION}\r\e[0K${DESCRIPTION:-SECTION}"
+    echo -e "\e[0Ksection_start:$(date +%s):${SECTION}\r\e[0K${DESCRIPTION:-$SECTION}"
 }
 
 ci_section_end() {
