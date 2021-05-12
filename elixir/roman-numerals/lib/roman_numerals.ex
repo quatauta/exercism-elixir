021a16540007f588677a1dc78180db67926dabf0
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-12 21:37:20 +0200

    Elixir/Roman Numerals: Implemented exercise based on previous solution

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,8 +1,26 @@
 defmodule RomanNumerals do
+  @symbols [[1000, "M" ],
+            [ 900, "CM"],
+            [ 500, "D" ],
+            [ 400, "CD"],
+            [ 100, "C" ],
+            [  90, "XC"],
+            [  50, "L" ],
+            [  40, "XL"],
+            [  10, "X" ],
+            [   9, "IX"],
+            [   5, "V" ],
+            [   4, "IV"],
+            [   1, "I" ],]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
+  def numeral(number) when number <= 0, do: ""
+
   def numeral(number) do
+    [value, symbol] = Enum.find(@symbols, 1, fn pair -> Enum.at(pair, 0) <= number end)
+    symbol <> numeral(number - value)
   end
 end
