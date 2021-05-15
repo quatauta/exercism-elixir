e37fa7c1910ce829a812de10865e0ead56ae98df
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-15 18:31:04 +0200

    Elixir/Pig Latin: Updated to new version using mix test

--- elixir/pig-latin/test/pig_latin_test.exs
+++ elixir/pig-latin/test/pig_latin_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("pig_latin.exs", __DIR__)
-end
-
-ExUnit.start()
-# ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule PigLatinTest do
   use ExUnit.Case
 
