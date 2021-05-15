e66c4a4d53e3bf3f00eb74b14e258cea41069177
Daniel Schömer <daniel.schoemer@gmx.net>
2021-05-15 09:04:50 +0200

    Elixir/Robot Simulator: Updated to new version using mix test

--- elixir/robot-simulator/test/robot_simulator_test.exs
+++ elixir/robot-simulator/test/robot_simulator_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("robot_simulator.exs", __DIR__)
-end
-
-ExUnit.start()
-# ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule RobotSimulatorTest do
   use ExUnit.Case
 
