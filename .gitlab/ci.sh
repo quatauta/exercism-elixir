5774bdd49b48d38ae65eb47d70fc259c1a6e5806
Daniel Schömer <daniel.schoemer@gmx.net>
2021-10-03 18:37:07 +0000

    Use scripts in CI

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-ci_section_start() {
-    SECTION="$1"
-    DESCRIPTION="$2"
-    echo -e "\e[0Ksection_start:$(date +%s):${SECTION}\r\e[0K${DESCRIPTION:-$SECTION}"
+ci_start() {
+    echo -e "\e[0Ksection_start:$(date +%s):${1}\r\e[0K${1}"
 }
 
-ci_section_end() {
-    SECTION="$1"
-    echo -e "\e[0Ksection_end:$(date +%s):${SECTION}\r\e[0K"
+ci_end() {
+    echo -e "\e[0Ksection_end:$(date +%s):${1}\r\e[0K"
+}
+
+ci_section() {
+    local SECTION=$1
+    shift
+    ci_start "${SECTION}"
+    "${@}"
+    ci_end "${SECTION}"
 }
