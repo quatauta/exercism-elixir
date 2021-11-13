aa855b55a127c620547211e8b914e4e1753eb4b8
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-11-13 21:22:05 +0100

    Sublist: Fix moduledoc to doc

--- sublist/lib/sublist.ex
+++ sublist/lib/sublist.ex
@@ -1,5 +1,5 @@
 defmodule Sublist do
-  @moduledoc """
+  @doc """
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
