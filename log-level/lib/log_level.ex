f57d256b905b4331cb1376fcdb838267134d7de2
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-11-14 20:24:58 +0100

    Log Level: Implement with cond are asked by Exercism

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,21 +1,26 @@
 defmodule LogLevel do
   @moduledoc false
 
-  @spec to_label(integer, boolean) :: :debug | :error | :fatal | :info | :trace | :unknown | :warning
-  def to_label(0, false), do: :trace
-  def to_label(0, true), do: :unknown
-  def to_label(1, _), do: :debug
-  def to_label(2, _), do: :info
-  def to_label(3, _), do: :warning
-  def to_label(4, _), do: :error
-  def to_label(5, false), do: :fatal
-  def to_label(5, true), do: :unknown
-  def to_label(_, _), do: :unknown
+  @spec to_label(integer, boolean) :: :trace | :debug | :info | :warning | :error | :fatal
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 && !legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && !legacy? -> :fatal
+      true -> :unknown
+    end
+  end
 
-  @spec alert_recipient(atom, boolean) :: :dev1 | :dev2 | nil | :ops
-  def alert_recipient(level, true) when level < 1 or level > 4, do: :dev1
-  def alert_recipient(level, false) when level < 0 or level > 5, do: :dev2
-  def alert_recipient(level, _) when level == 4, do: :ops
-  def alert_recipient(level, _) when level == 5, do: :ops
-  def alert_recipient(_, _), do: nil
+  @spec alert_recipient(integer, boolean) :: :dev1 | :dev2 | :ops | nil
+  def alert_recipient(level, legacy?) do
+    cond do
+      legacy? && (level < 1 || 4 < level) -> :dev1
+      !legacy? && (level < 0 || 5 < level) -> :dev2
+      level in [4, 5] -> :ops
+      true -> nil
+    end
+  end
 end
