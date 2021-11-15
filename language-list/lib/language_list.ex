8d61be57308df9f3179c1bdd08c94b7fb14fd727
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-11-15 08:03:46 +0100

    Language List: Add solved exercise

---
+++ language-list/lib/language_list.ex
@@ -0,0 +1,36 @@
+defmodule LanguageList do
+  @moduledoc false
+
+  @spec new :: []
+  def new() do
+    []
+  end
+
+  @spec add(list, any) :: nonempty_maybe_improper_list
+  def add(list, language) do
+    [language | list]
+  end
+
+  @spec remove(maybe_improper_list) :: any
+  def remove([]), do: []
+  def remove([_ | tail]) do
+    tail
+  end
+
+  @spec first(maybe_improper_list) :: any
+  def first([]), do: nil
+  def first([head | _]) do
+    head
+  end
+
+  @spec count(list) :: non_neg_integer
+  def count([]), do: 0
+  def count([_ | tail]) do
+    1 + count(tail)
+  end
+
+  @spec exciting_list?(maybe_improper_list) :: boolean
+  def exciting_list?([]), do: false
+  def exciting_list?(["Elixir" | _]), do: true
+  def exciting_list?([_ | tail]), do: exciting_list?(tail)
+end
