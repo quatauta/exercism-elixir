35eebb92fc2a1ed6dfeea91d555280cabcceb4bd
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-11-19 09:07:30 +0100

    German Sysadmin: Solve exercise using case

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -3,13 +3,16 @@ defmodule Username do
 
   @spec sanitize(username :: charlist()) :: charlist()
   def sanitize([]), do: []
-  def sanitize([first | tail]) when first == ?ä, do: 'ae' ++ sanitize(tail)
-  def sanitize([first | tail]) when first == ?ö, do: 'oe' ++ sanitize(tail)
-  def sanitize([first | tail]) when first == ?ü, do: 'ue' ++ sanitize(tail)
-  def sanitize([first | tail]) when first == ?ß, do: 'ss' ++ sanitize(tail)
 
-  def sanitize([first | tail]) when first == ?_ or (?a <= first and first <= ?z),
-    do: [first | sanitize(tail)]
-
-  def sanitize([_ | tail]), do: sanitize(tail)
+  def sanitize([first | tail]) do
+    case first do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      ?_ -> '_'
+      first when ?a <= first and first <= ?z -> [first]
+      _ -> ''
+    end ++ sanitize(tail)
+  end
 end
