40bf5a7b6ad8c93ff927a5d883f8d0d3e09287af
Daniel Schömer <daniel.schoemer@gmx.net>
2021-11-30 07:27:15 +0000

    Remote control car

---
+++ remote-control-car/lib/remote_control_car.ex
@@ -0,0 +1,37 @@
+defmodule RemoteControlCar do
+  @moduledoc false
+
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
+
+  @type t :: %RemoteControlCar{}
+
+  @spec new :: t()
+  def new(), do: new("none")
+
+  @spec new(nickname :: String.t()) :: RemoteControlCar.t()
+  def new(nickname), do: %RemoteControlCar{nickname: nickname}
+
+  @spec display_distance(remote_car :: RemoteControlCar.t()) :: String.t()
+  def display_distance(%RemoteControlCar{} = remote_car) do
+    "#{remote_car.distance_driven_in_meters} meters"
+  end
+
+  @spec display_battery(remote_car :: RemoteControlCar.t()) :: String.t()
+  def display_battery(%RemoteControlCar{battery_percentage: 0}), do: "Battery empty"
+
+  def display_battery(%RemoteControlCar{} = remote_car) do
+    "Battery at #{remote_car.battery_percentage}%"
+  end
+
+  @spec drive(remote_car :: RemoteControlCar.t()) :: RemoteControlCar.t()
+  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car), do: remote_car
+
+  def drive(%RemoteControlCar{} = remote_car) do
+    %{
+      remote_car
+      | battery_percentage: remote_car.battery_percentage - 1,
+        distance_driven_in_meters: remote_car.distance_driven_in_meters + 20
+    }
+  end
+end
