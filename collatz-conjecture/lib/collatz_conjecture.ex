4efc69ed3bf174f6984c50f2e4628ed4e417f7d0
Daniel Schömer <daniel.schoemer@gmx.net>
2021-12-13 07:09:05 +0000

    Collatz Conjecture

---
+++ collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,20 @@
+defmodule CollatzConjecture do
+  @moduledoc false
+
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(1), do: 0
+
+  def calc(input) when is_integer(input) and input > 1 do
+    if rem(input, 2) == 1 do
+      1 + calc(input * 3 + 1)
+    else
+      1 + calc(div(input, 2))
+    end
+  end
+end
