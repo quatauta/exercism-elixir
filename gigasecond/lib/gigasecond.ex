0a7330fcf2f2a2929695fb8afc61c617e5bf4068
Daniel Schömer <daniel.schoemer@gmx.net>
2021-12-20 13:54:49 +0000

    Gigasecond: Use NativeDateTime.from_erl!/3

--- gigasecond/lib/gigasecond.ex
+++ gigasecond/lib/gigasecond.ex
@@ -4,15 +4,11 @@ defmodule Gigasecond do
   @doc """
   Calculate a date one billion seconds after an input date.
   """
-  @spec from(
-          {{year :: pos_integer, month :: pos_integer, day :: pos_integer},
-           {hour :: pos_integer, minute :: pos_integer, second :: pos_integer}}
-        ) ::
-          {{year :: pos_integer, month :: pos_integer, day :: pos_integer},
-           {hour :: pos_integer, minute :: pos_integer, second :: pos_integer}}
-  def from({{_, _, _} = date, {_, _, _} = time}) do
-    datetime = DateTime.new!(Date.from_erl!(date), Time.from_erl!(time))
-    future = DateTime.add(datetime, 1_000_000_000)
-    {Date.to_erl(future), Time.to_erl(future)}
+  @spec from(File.erlang_time()) :: File.erlang_time()
+  def from({{_, _, _}, {_, _, _}} = date) do
+    date
+    |> NaiveDateTime.from_erl!()
+    |> NaiveDateTime.add(1_000_000_000)
+    |> NaiveDateTime.to_erl()
   end
 end
