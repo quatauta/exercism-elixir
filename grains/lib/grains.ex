7e80767bcf975697462f5c3c59c9b0cac96cebe5
Daniel Schömer <daniel.schoemer@gmx.net>
2021-12-22 11:32:06 +0000

    Grains

---
+++ grains/lib/grains.ex
@@ -0,0 +1,16 @@
+defmodule Grains do
+  @moduledoc false
+
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
+  def square(number) when 1 <= number and number <= 64, do: {:ok, 2 ** (number - 1)}
+  def square(_), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: {:ok, pos_integer()}
+  def total, do: {:ok, 2 ** 64 - 1}
+end
