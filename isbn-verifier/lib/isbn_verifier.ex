a06e626e99615a39c1a2e8d34802723476f49b4c
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-12-22 21:30:55 +0100

    ISBN Verifier: Satisfy tests

--- isbn-verifier/lib/isbn_verifier.ex
+++ isbn-verifier/lib/isbn_verifier.ex
@@ -13,5 +13,18 @@ defmodule IsbnVerifier do
   """
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
+    dashless = isbn |> String.replace("-", "") |> String.downcase()
+
+    if String.match?(dashless, ~r/^[0-9]{9}[0-9x]$/) do
+      dashless
+      |> String.graphemes()
+      |> Enum.map(&to_integer/1)
+      |> Enum.zip(10..1)
+      |> Enum.reduce(0, fn {digit, multiplier}, acc -> acc + digit * multiplier end)
+      |> Integer.mod(11) == 0
+    end
   end
+
+  defp to_integer("x"), do: 10
+  defp to_integer(digit), do: String.to_integer(digit)
 end
