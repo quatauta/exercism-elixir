e9ecc4cd0734a462175ca1e6f316f19ced091641
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-12-22 12:30:09 +0100

    Grains: mix format

--- grains/lib/grains.ex
+++ grains/lib/grains.ex
@@ -6,10 +6,9 @@ defmodule Grains do
   def square(number) when 1 <= number and number <= 64, do: {:ok, 2 ** (number - 1)}
   def square(_), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
 
-
   @doc """
   Adds square of each number from 1 to 64.
   """
   @spec total :: {:ok, pos_integer()}
-  def total, do: {:ok, 2**64 - 1}
+  def total, do: {:ok, 2 ** 64 - 1}
 end
