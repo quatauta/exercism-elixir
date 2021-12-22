1066790583eac01123f86fccc8bd2c86680b3759
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-12-22 12:29:43 +0100

    Grains: Satisfy tests

--- grains/lib/grains.ex
+++ grains/lib/grains.ex
@@ -3,13 +3,13 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
-  def square(number) do
-  end
+  def square(number) when 1 <= number and number <= 64, do: {:ok, 2 ** (number - 1)}
+  def square(_), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
+
 
   @doc """
   Adds square of each number from 1 to 64.
   """
   @spec total :: {:ok, pos_integer()}
-  def total do
-  end
+  def total, do: {:ok, 2**64 - 1}
 end
