a14d7111d9a2a7e41b6e54fe8cb36f5f83eb7c74
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-12-23 19:44:50 +0100

    Kindergarten Garden: Satisfy tests

--- kindergarten-garden/lib/garden.ex
+++ kindergarten-garden/lib/garden.ex
@@ -1,6 +1,8 @@
 defmodule Garden do
   @moduledoc false
 
+  @plants %{ "C" => :clover, "G" => :grass, "R" => :radishes, "V" => :violets }
+
   @doc """
     Accepts a string representing the arrangement of cups on a windowsill and a
     list with names of students in the class. The student names list does not
@@ -11,6 +13,25 @@ defmodule Garden do
   """
 
   @spec info(String.t(), list) :: map
-  def info(info_string, student_names) do
+  def info(info_string, student_names \\ [ :alice, :bob, :charlie, :david, :eve, :fred, :ginny, :harriet, :ileana, :joseph, :kincaid, :larry ]) do
+    garden = info_string |> String.split()
+    student_names
+    |> Enum.sort()
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {name, index}, map -> Map.put(map, name, plants_at(garden, index)) end )
+  end
+
+  defp plants_at(garden, index) do
+    a = garden |> Enum.at(0) |> String.at(index * 2)
+    b = garden |> Enum.at(0) |> String.at(index * 2 + 1)
+    c = garden |> Enum.at(1) |> String.at(index * 2)
+    d = garden |> Enum.at(1) |> String.at(index * 2 + 1)
+
+    [
+    Map.get(@plants, a),
+    Map.get(@plants, b),
+    Map.get(@plants, c),
+    Map.get(@plants, d)
+    ] |> Enum.filter(&(&1)) |> List.to_tuple()
   end
 end
