361fa1effcdef6c9501f15eb42b0abbaf1440100
Daniel Schoemer <daniel.schoemer@gmx.net>
2021-12-25 08:58:43 +0100

    Matching Brackets: Satisfy tests

--- matching-brackets/lib/matching_brackets.ex
+++ matching-brackets/lib/matching_brackets.ex
@@ -6,5 +6,22 @@ defmodule MatchingBrackets do
   """
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
+    helper(str, [])
   end
+
+  defp helper(<<>>, brackets), do: Enum.empty?(brackets)
+
+  defp helper(<<?(, rest::binary>>, brackets), do: helper(rest, ["(" | brackets])
+  defp helper(<<?{, rest::binary>>, brackets), do: helper(rest, ["{" | brackets])
+  defp helper(<<?[, rest::binary>>, brackets), do: helper(rest, ["[" | brackets])
+
+  defp helper(<<?), rest::binary>>, ["(" | brackets]), do: helper(rest, brackets)
+  defp helper(<<?}, rest::binary>>, ["{" | brackets]), do: helper(rest, brackets)
+  defp helper(<<?], rest::binary>>, ["[" | brackets]), do: helper(rest, brackets)
+
+  defp helper(<<?), _::binary>>, _), do: false
+  defp helper(<<?}, _::binary>>, _), do: false
+  defp helper(<<?], _::binary>>, _), do: false
+
+  defp helper(<<_, rest::binary>>, brackets), do: helper(rest, brackets)
 end
