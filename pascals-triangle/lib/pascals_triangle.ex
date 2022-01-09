bbfeb39ffadad8ab3621b1bd89a85385f039246f
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-01-09 21:04:12 +0100

    Pascals Triangle: mix format

--- pascals-triangle/lib/pascals_triangle.ex
+++ pascals-triangle/lib/pascals_triangle.ex
@@ -7,12 +7,13 @@ defmodule PascalsTriangle do
   """
   @spec rows(integer) :: [[integer]]
   def rows(num) do
-    [1] |> Stream.unfold(&({&1, next_row(&1)})) |> Enum.take(num)
+    [1] |> Stream.unfold(&{&1, next_row(&1)}) |> Enum.take(num)
   end
 
   defp next_row(row) do
     next_row = Enum.concat(row, [1])
     length = length(next_row)
+
     Enum.with_index(next_row, fn element, index ->
       if index > 0 && index < length - 1 do
         Enum.at(next_row, index - 1) + element
