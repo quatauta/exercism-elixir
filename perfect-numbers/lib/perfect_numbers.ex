05bd75119dbee46b9b088a54561254393ae20aea
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-01-10 21:17:25 +0100

    Perfect Numbers: Satisfy tests

--- perfect-numbers/lib/perfect_numbers.ex
+++ perfect-numbers/lib/perfect_numbers.ex
@@ -12,6 +12,20 @@ defmodule PerfectNumbers do
   :deficient if the aliquot sum is less than `number`
   """
   @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
-  def classify(number) do
+  def classify(number) when number <= 0, do: {:error, "Classification is only possible for natural numbers."}
+  def classify(number) when number >= 1 do
+    aliquot_sum = aliquot_sum(number)
+    cond do
+      aliquot_sum == number -> {:ok, :perfect}
+      aliquot_sum < number -> {:ok, :deficient}
+      number < aliquot_sum -> {:ok, :abundant}
+    end
   end
+
+  defp aliquot_sum(number), do: number |> factors() |> Enum.reverse() |> tl() |> Enum.sum()
+  defp factors(n), do: n |> factors(1, []) |> Enum.sort()
+  defp factors(n, i, factors) when n < i * i, do: factors
+  defp factors(n, i, factors) when n == i * i, do: [i | factors]
+  defp factors(n, i, factors) when rem(n, i) == 0, do: factors(n, i + 1, [i, div(n, i) | factors])
+  defp factors(n, i, factors), do: factors(n, i + 1, factors)
 end
