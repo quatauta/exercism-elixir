af9a130cd88f5bea70998f972c9c88dab2c6607c
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-01-13 20:42:23 +0100

    Prime Factors: Improve speed for large numbers

--- prime-factors/lib/prime_factors.ex
+++ prime-factors/lib/prime_factors.ex
@@ -15,7 +15,7 @@ defmodule PrimeFactors do
   end
 
   defp factors(1, _), do: []
-
+  defp factors(n, divisor) when divisor * divisor > n, do: [n]
   defp factors(n, divisor) do
     if rem(n, divisor) == 0 do
       [divisor | factors(div(n, divisor), divisor)]
