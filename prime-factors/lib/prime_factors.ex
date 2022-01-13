860f1d124a6bc51a065bad6ce4a3b0441a8e10fd
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-01-13 20:35:18 +0100

    Prime Factors: Satisfy tests

--- prime-factors/lib/prime_factors.ex
+++ prime-factors/lib/prime_factors.ex
@@ -10,6 +10,16 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(number) do
+  def factors_for(n) do
+    factors(n, 2)
+  end
+
+  defp factors(1, _), do: []
+  defp factors(n, divisor) do
+    if rem(n, divisor) == 0 do
+      [divisor | factors(div(n, divisor), divisor)]
+    else
+      factors(n, divisor + 1)
+    end
   end
 end
