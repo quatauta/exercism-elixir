f6ce9f3f9c83a178220a762245b24fce5f890e3e
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-01-14 20:41:31 +0100

    Proverb: Satisfy tests

--- proverb/lib/proverb.ex
+++ proverb/lib/proverb.ex
@@ -5,6 +5,12 @@ defmodule Proverb do
   Generate a proverb from a list of strings.
   """
   @spec recite(strings :: [String.t()]) :: String.t()
-  def recite(strings) do
+  def recite([]), do: ""
+  def recite([head | _] = strings) do
+    for_want = strings |> Enum.chunk_every(2, 1, :discard) |> Enum.map(&for_want/1)
+    Enum.join(for_want ++ [and_all(head)], "")
   end
+
+  defp for_want([first | [second]]), do: "For want of a #{first} the #{second} was lost.\n"
+  defp and_all(string), do: "And all for the want of a #{string}.\n"
 end
