583c9e7c6d961f8618fcbd46c654fdd7fffcb417
Daniel Schoemer <daniel.schoemer@gmx.net>
2022-02-04 21:39:21 +0100

    Complex Numbers: Fix add/2, sub/2, mul/2, div/2

--- complex-numbers/lib/complex_numbers.ex
+++ complex-numbers/lib/complex_numbers.ex
@@ -25,32 +25,37 @@ defmodule ComplexNumbers do
   Multiply two complex numbers, or a real and a complex number
   """
   @spec mul(a :: complex | float, b :: complex | float) :: complex
-  def mul({r1, i1}, {r2, i2}) do
-    {r1 * r2 - i1 * i2, i2 * r2 + r1 * i2}
-  end
+  def mul({r1, i1}, {r2, i2}), do: {r1 * r2 - i1 * i2, i1 * r2 + r1 * i2}
+  def mul(a, {r2, i2}), do: {r2 * a, i2 * a}
+  def mul({r1, i1}, b), do: {r1 * b, i1 * b}
 
   @doc """
   Add two complex numbers, or a real and a complex number
   """
   @spec add(a :: complex | float, b :: complex | float) :: complex
-  def add({r1, i1}, {r2, i2}) do
-    {r1 + r2, i1 + i2}
-  end
+  def add({r1, i1}, {r2, i2}), do: {r1 + r2, i1 + i2}
+  def add(a, {r2, i2}), do: add({a, 0.0}, {r2, i2})
+  def add({r1, i1}, b), do: add({r1, i1}, {b, 0.0})
 
   @doc """
   Subtract two complex numbers, or a real and a complex number
   """
   @spec sub(a :: complex | float, b :: complex | float) :: complex
-  def sub({r1, i1}, {r2, i2}) do
-    {r1 - r2, i1 - i2}
-  end
+  def sub({r1, i1}, {r2, i2}), do:  {r1 - r2, i1 - i2}
+  def sub(a, {r2, i2}), do: sub({a, 0.0}, {r2, i2})
+  def sub({r1, i1}, b), do: sub({r1, i1}, {b, 0.0})
 
   @doc """
   Divide two complex numbers, or a real and a complex number
   """
   @spec div(a :: complex | float, b :: complex | float) :: complex
-  def div(a, b) do
+  # `(a + i * b) / (c + i * d) = (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i`.
+  def div({r1, i1}, {r2, i2}) do
+    { (r1 * r2 + i1 * i2) / (r2 ** 2 + i2 ** 2),
+      (i1 * r2 - r1 * i2) / (r2 ** 2 + i2 ** 2) }
   end
+  def div(a, {r2, i2}), do: __MODULE__.div({a, 0.0}, {r2, i2})
+  def div({r1, i1}, b), do: __MODULE__.div({r1, i1}, {b, 0.0})
 
   @doc """
   Absolute value of a complex number
