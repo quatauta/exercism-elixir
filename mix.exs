e05fcaefb5c82c40498631f18a26d6419a90681e
Daniel Schömer <daniel.schoemer@gmx.net>
2022-03-14 19:35:55 +0000

    Dependencies version bump 2022 03 14

--- mix.exs
+++ mix.exs
@@ -29,10 +29,10 @@ defmodule ExercismElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:covertool, "~> 2.0", only: [:test], runtime: false},
-      {:junit_formatter, "~> 3.1", only: [:test]},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:junit_formatter, "~> 3.3", only: [:test]},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
     ]
   end
 end
